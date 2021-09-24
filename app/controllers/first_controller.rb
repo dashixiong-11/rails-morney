5fa3723af18c85daeda937731c6fffabcea6184a
zjw <changewhen11@gamil.com>
2021-09-24 10:39:46 +0800

    添加 i18n

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -2,5 +2,4 @@ class FirstController < ApplicationController
   def hello
     render 'first/hello'
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def create
-    user = User.new
-    user.email = params[:email]
-    user.password = params[:password]
-    user.password_confirmation = params[:password_confirmation]
+    user = User.new({ email: params[:email], password: params[:password], password_confirm: params[:password_confirm] })
+    # user.email = params[:email]
+    # user.password = params[:password]
+    # user.password_confirmation = params[:password_confirmation]
     user.save
+    if user.save
+      render json: { resource: user }, status: 200
+    else
+      render json: { errors: user.errors }, status: 400
+    end
     p user.errors
   end
 end
