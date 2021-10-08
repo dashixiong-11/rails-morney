01ca18e7453679e99081989c88bda608902d8de7
zjw <changewhen11@gamil.com>
2021-10-08 18:10:33 +0800

    优化 user 代码

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
   def create
-    user = User.new({ email: params[:email], password: params[:password], password_confirm: params[:password_confirm] })
     # user.email = params[:email]
     # user.password = params[:password]
     # user.password_confirmation = params[:password_confirmation]
-    user.save
-    if user.save
-      render json: { resource: user }, status: 200
-    else
-      render json: { errors: user.errors }, status: 400
-    end
-    p user.errors
+    render_resource User.create create_params
+  end
+
+  def create_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
