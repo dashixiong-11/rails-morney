33540e3343557dc6f7796791069de50afc8838d3
zjw <changewhen11@gamil.com>
2021-10-13 14:17:13 +0800

    验证邮箱的唯一性

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
     # user.email = params[:email]
     # user.password = params[:password]
     # user.password_confirmation = params[:password_confirmation]
-    user = User.create create_params
     render_resource User.create create_params
-    UserMailer.welcome_email(user).deliver_now
   end
 
   def create_params
+    p 'params'
+    p params
     params.permit(:email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+
   validates_presence_of :email
+  validates_uniqueness_of :email
   validates_presence_of :password_confirmation, on: [:create]
   validates_format_of :email, with: /.+@.+/, if: :email
-  validates :password, { minimum: 6 }, on: [:create], if: :password
+  validates :password, length: { minimum: 6 }, on: [:create], if: :password
+
+  after_create :send_welcome_email #当user被成功创建后调用
+
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver_later
+  end
+
 end
