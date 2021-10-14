f139cc341a7a4ebe4063b1a596252adfc43e5f37
zjw <changewhen11@gamil.com>
2021-10-14 19:14:59 +0800

    添加自定义校验

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,4 +5,14 @@ class Session
   validates :password, presence: true
   validates_format_of :email, with: /.+@.+/, if: :email
   validates :password, length: { minimum: 6 }, if: :password
+
+  validate :check_email, if: :email
+
+  def check_email
+    user = User.find_by email: email
+    if user.nil?
+      errors.add :email, :not_fund
+    end
+
+  end
 end
