221472504727e19df8bd781cbf92ff8c88836219
zjw <changewhen11@gamil.com>
2021-10-15 16:11:21 +0800

    添加session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     s = Session.new create_params #直接new 不会触发校验
     s.validate
     render_resource s #  new一个session 如果通过验证就返回session对象，如果失败就返回错误， 跟user的create 对应 只不过不存入数据库
-    # session[:current_user] = session.user
+    session[:current_user_id] = s.user.id
   end
 
   def destroy
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,7 +1,11 @@
 class Session
   include ActiveModel::Model
-  attr_accessor :email, :password
-
+  attr_accessor :email, :password, :user
+  # attr_accessor :email
+  # 等价于
+  # @email 声明属性
+  # def email 获取 @email 的值
+  # def email= 给@email 赋值
   validates :email, presence: true
   validate :check_email, if: :email
   validates :password, presence: true
@@ -11,14 +15,14 @@ class Session
   validate :email_password_match, if: Proc.new { |s| s.email.present? and s.password.present? }
 
   def check_email
-    user ||= User.find_by_email email
+    @user ||= User.find_by_email email #已经默认声明过有一个 @user 属性了 所以可以直接用
     if user.nil?
       errors.add :email, :not_fund
     end
   end
 
   def email_password_match
-    user ||= User.find_by_email email
+    @user ||= User.find_by_email email
     if user and not user.authenticate(password)
       errors.add :password, :mismatch
     end
