1e485d4dbaeafda5ef178048b5c1e1968f98dd44
zjw <changewhen11@gamil.com>
2021-10-15 15:30:14 +0800

    添加自定义校验 密码

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,18 +1,26 @@
 class Session
   include ActiveModel::Model
   attr_accessor :email, :password
+
   validates :email, presence: true
+  validate :check_email, if: :email
   validates :password, presence: true
+
   validates_format_of :email, with: /.+@.+/, if: :email
   validates :password, length: { minimum: 6 }, if: :password
-
-  validate :check_email, if: :email
+  validate :email_password_match, if: Proc.new { |s| s.email.present? and s.password.present? }
 
   def check_email
-    user = User.find_by email: email
+    user ||= User.find_by_email email
     if user.nil?
       errors.add :email, :not_fund
     end
+  end
 
+  def email_password_match
+    user ||= User.find_by_email email
+    if user and not user.authenticate(password)
+      errors.add :password, :mismatch
+    end
   end
 end
